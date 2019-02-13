c8f29e04bf296a0c2f9753e3bb11bb9c3a2abd89
kishori <kishori@kishori-desktop>
2019-02-13 18:37:54 +0530

    Final changes

--- run_hindi_parser.sh
+++ run_hindi_parser.sh
@@ -1,11 +1,8 @@
 #sh run_hindi_parser.sh /path/hindi_wx_input /path/hindi_new_out
-#If demo file is in same directory i.e. csnlp:
-#sh run_hindi_parser.sh demo demo.out
-#If demo file is in different directory i.e. csnlp:
-#sh run_hindi_parser.sh ../demo ../demo.out
+#sh run_hindi_parser.sh /home/kishori/csnlp/demo demo.out
 
 
-input_file=$hindi_parser/$1  #tmp_anu_dir/tmp/oneE_tmp/oneH  ........ when called from Anusaaraka_alignment.sh
+input_file=$1  #tmp_anu_dir/tmp/oneE_tmp/oneH  ........ when called from Anusaaraka_alignment.sh
 output_file=$2 #tmp_anu_dir/tmp/oneE_tmp/hindi_dep_parser_out.txt ............... when called from Anusaaraka_alignment.sh
 
 
@@ -19,7 +16,7 @@ echo "Tokenized sentences are:"
 cat $hindi_parser/hindi_wx_tokenised
 
 cd $hindi_parser   #Go to hindi parser directory
-/usr/bin/python mono_jm_parser.py --load $hindi_parser/PARSER/UD/HI/PARSER/hi-ud-parser --test $hindi_parser/hindi_wx_tokenised --output-file $output_file
+python mono_jm_parser.py --load $hindi_parser/PARSER/UD/HI/PARSER/hi-ud-parser --test $hindi_parser/hindi_wx_tokenised --output-file $output_file
 #/home/kishori/csnlp/PARSER/UD/HI/PARSER/hi-ud-parser
 echo "Parsed sentences are:"
 cat $output_file
@@ -31,4 +28,3 @@ cat $output_file
 #isc-tokenizer  -i $hindi_parser/$input_file -o $hindi_parser/hindi_wx_tokenised
 
 #python mono_jm_parser.py --load $hindi_parser/PARSER/UD/HI/PARSER/hi-ud-parser --test hindi_wx_tokenised --output-file hindi_parse
-
